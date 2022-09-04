79e8c78636d5f5233ada535b36131b7598aaeaa7
Rahul Kumar <Rahulkr9622@gmail.com>
2022-09-04 11:23:05 +0530

    Enabling code quality params
    Now we have enabled:
    -> no duplicates (beyond 3 lines)
    -> CCN not allowed beyond 2
    -> Coverage for tests
    
    [Ticket: X]

--- helper_scripts/check_range.py
+++ helper_scripts/check_range.py
@@ -1,4 +1,8 @@
 def is_in_range(present_value, min_value, max_value):
-    if (present_value < min_value or present_value > max_value):
-        return False
-    return True
+    return is_not_greater(present_value, max_value) and is_not_lesser(present_value, min_value)
+
+def is_not_greater(present_value, max_value):
+    return (present_value < max_value)
+
+def is_not_lesser(present_value, min_value):
+    return (present_value > min_value)
