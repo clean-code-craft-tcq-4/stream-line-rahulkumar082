6defc779c909b1abcf8c3a850aafa75e1f21f3ec
Rahul Kumar <rahulkr9622@gmail.com>
2022-09-14 11:24:15 +0530

    Update sender.py

--- stream_bms_data/sender/sender.py
+++ stream_bms_data/sender/sender.py
@@ -14,7 +14,11 @@ def verify_csv_data_with_sensors():
         is_temp_ok = check_if_sensor_okay(Temperature, temperature_value)
         is_soc_ok = check_if_sensor_okay(State_Of_Charge, soc_value)
         verified_csv_data.append([is_temp_ok, is_soc_ok])
+    console_print(verified_csv_data)
     return (verified_csv_data)
 
 def check_if_sensor_okay(sensor_class, value):
     return value if sensor_class().validate(value) else False
+
+def console_print(verified_csv_data):
+    print(verified_csv_data)
