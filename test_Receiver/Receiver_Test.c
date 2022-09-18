bbdb795c70d2ee0ab1d5b78a1425e919fd7fcf58
ajusugathan <38355874+ajusugathan@users.noreply.github.com>
2022-09-18 20:48:48 +0530

    Create Receiver_Test.c

---
+++ test_Receiver/Receiver_Test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "Receiver/receiver.h"
+
+void TEST_CASE_01()
+{
+	printf("\n Test case to console read of temperature and soc values\n");
+  float temperature_1[10],soc_1[10];
+  int size=2;
+  assert(ReadConsoleData(float *temperature,float *soc,int size)==size);
+  printf("\n read data from console");
+  for(int i=0;i<size;i++)
+  {
+    printf("%f , %f ",temperature_1[i],soc_1[i]);
+  }
+}
+
+int main()
+{
+   TEST_CASE_01();
+}
